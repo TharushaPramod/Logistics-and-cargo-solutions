199f8f3226c4d178ed433add214dfea7e17eee52
TharushaPramod <pramodtharusha@gmail.com>
2026-02-11 17:37:21 +0530

    feat : create routes path
    feat : create routes path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,9 @@ import Hero from "./components/Hero"
 import About from "./pages/About"
 import Services from "./pages/Services"
 import Features from "./pages/Features"
+import Gallery from "./pages/Gallery"
+import Contact from "./pages/Contact"
+import Footer from "./components/Footer"
 
 
 function App() {
@@ -16,7 +19,13 @@ function App() {
          <Route path="/about" element={<About/>}></Route>
          <Route path="/service" element={<Services/>}></Route>
            <Route path="/feature" element={<Features/>}></Route>
+           <Route path="/gallery" element={<Gallery/>}></Route>
+          <Route path="/contact" element={<Contact/>}></Route>
+
+           
       </Routes>
+
+      <Footer/>
 
 
    
